158ad7ef18e92b7e639e33d4873168d0c9418f15
tapirCode <tapircode@DESKTOP-L5O4IDJ.localdomain>
2026-06-28 17:09:44 +0800

    add projeck

---
+++ dataCardProjeck.js
@@ -0,0 +1,64 @@
+const dataCard =[
+    {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/image-tapir.png",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        linkUrl:"tapircode.com"
+    },
+]
+
+export default dataCard 
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const menuButton = document.querySelector(".menu-button")
 const menuNavbar =document.querySelector(".menu-navbar")
+const sectionCard =document.querySelector(".section-card")
+import datacard from "./dataCardProjeck.js" 
 
 menuButton.addEventListener("click", ()=>{
 menuNavbar.classList.toggle("active")
+})
+
+datacard.forEach(datacard => {
+    sectionCard.innerHTML+=`
+    <div class="cardList">
+    <img src="${datacard.imageUrl}">
+    <h4>${datacard.judul}</h4>
+    <p>${datacard.deskripsi}</p>
+    <button>${datacard.linkUrl}</button>`
 })
