6f6b8f44a8de1e7c844471afd727430c28244b36
tapirCode <tapircode@DESKTOP-L5O4IDJ.localdomain>
2026-06-29 00:53:13 +0800

    Update

--- dataCardProjeck.js
+++ dataCardProjeck.js
@@ -2,61 +2,61 @@ const dataCard =[
     {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
-        linkUrl:"tapircode.com"
-    },
-      {
-        imageUrl:"./img/image-tapir.png",
-        judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat",
         linkUrl:"tapircode.com"
     },
       {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+        linkUrl:"tapircode.com",
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing eInlit.",
+        linkUrl:"tapircode.com",
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
+        linkUrl:"tapircode.com",
+    },
+      {
+        imageUrl:"./img/dummyimage.jpg",
+        judul:"Sistem pos kasir",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
         linkUrl:"tapircode.com"
     },
       {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
         linkUrl:"tapircode.com"
     },
       {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
         linkUrl:"tapircode.com"
     },
       {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
         linkUrl:"tapircode.com"
     },
       {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
         linkUrl:"tapircode.com"
     },
       {
         imageUrl:"./img/dummyimage.jpg",
         judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
-        linkUrl:"tapircode.com"
-    },
-      {
-        imageUrl:"./img/dummyimage.jpg",
-        judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
-        linkUrl:"tapircode.com"
-    },
-      {
-        imageUrl:"./img/dummyimage.jpg",
-        judul:"Sistem pos kasir",
-        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu Ad litora torquent per conubia nostra inceptos himenaeos",
+        deskripsi:"Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat.",
         linkUrl:"tapircode.com"
     },
 ]
